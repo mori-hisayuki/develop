244dbdba5a4e5c04435860080797a39e06c9ea19
hisayuki_mori <h.mori0206@gmail.com>
2022-06-26 16:28:46 +0900

    format fix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,37 +1,37 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solidPlugin()],
   // server設定を追加、portは3030に固定
   server: {
-    port: 3030,
+    port: 3030
   },
   test: {
     environment: 'jsdom',
     globals: true,
     transformMode: {
-      web: [/\.[jt]sx?$/],
+      web: [/\.[jt]sx?$/]
     },
     setupFiles: './setupVitest.ts',
     // solid needs to be inline to work around
     // a resolution issue in vitest:
     deps: {
-      inline: [/solid-js/],
+      inline: [/solid-js/]
     },
     // if you have few tests, try commenting one
     // or both out to improve performance:
     threads: false,
-    isolate: false,
+    isolate: false
   },
   build: {
     target: 'esnext',
-    polyfillDynamicImport: false,
+    polyfillDynamicImport: false
   },
   resolve: {
-    conditions: ['development', 'browser'],
+    conditions: ['development', 'browser']
   }
-});
+})
